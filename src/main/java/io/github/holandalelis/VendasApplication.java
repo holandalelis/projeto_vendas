94b1c94b20cacae78972d028c33b60bd2bed893d
pedro <pedrolelis@hotmail.com>
2022-01-25 23:48:15 -0300

    inicializa branch para trabalhar com base de dados

--- src/main/java/io/github/holandalelis/VendasApplication.java
+++ src/main/java/io/github/holandalelis/VendasApplication.java
@@ -1,7 +1,6 @@
 package io.github.holandalelis;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VendasApplication {
