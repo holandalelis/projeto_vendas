3824583ebf788907aac83dfa925182b5552229f1
pedro <pedrolelis@hotmail.com>
2022-01-27 23:51:39 -0300

    update and create classes and functions

--- src/main/java/io/github/holandalelis/VendasApplication.java
+++ src/main/java/io/github/holandalelis/VendasApplication.java
@@ -1,10 +1,26 @@
 package io.github.holandalelis;
 
+import io.github.holandalelis.domain.entity.Cliente;
+import io.github.holandalelis.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Pedro");
+            clientes.salvar(cliente);
+        };
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
