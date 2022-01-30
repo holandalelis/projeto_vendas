42b0fc6b20dadc937dc5db077dc82b79a498f562
pedro <pedrolelis@hotmail.com>
2022-01-30 18:35:16 -0300

    mapping entity Cliente

--- src/main/java/io/github/holandalelis/domain/entity/Cliente.java
+++ src/main/java/io/github/holandalelis/domain/entity/Cliente.java
@@ -11,6 +11,7 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
+    @Column(name="nome", length = 100)
     private String nome;
 
     public Cliente() {
